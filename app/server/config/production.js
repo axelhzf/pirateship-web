0a531e6693b1f2f169a6051894c51db0e22d3cd1
Axel Hernández Ferrera <axelhzf@gmail.com>
2014-12-29 18:41:02 +0000

    production config

--- app/server/config/production.js
+++ app/server/config/production.js
@@ -2,7 +2,6 @@ var path = require("path");
 
 module.exports = {
   postProcess: {
-    enable: true,
     downloadedFolder: "/Volumes/Datos/Downloads/downloaded/",
     destinationFolder: "/Volumes/Datos/Video/"
   }
