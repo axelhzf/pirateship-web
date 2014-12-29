fe1aa0ca3479d97bd0b98bc57c9b729eae157501
Axel Hernández Ferrera <axelhzf@gmail.com>
2014-12-29 17:43:45 +0000

    post process

--- app/server/server.js
+++ app/server/server.js
@@ -1,8 +1,16 @@
 var barbakoa = require("barbakoa");
+var events = barbakoa.events;
 var router = barbakoa.router;
+var postProcess = require("./services/postProcess");
+var queue = require("./queue");
 
 var app = new barbakoa();
 
 require("./routes");
+
+events.on("post-start", function * () {
+  yield postProcess.start();
+  queue.start();
+});
 
 app.start();
